947f4c77f9e5aa06b2b06caae8ecd45d9da504a9
松山 和矢 <rorokage@gmail.com>
2015-10-14 17:26:48 +0900

    マクロの勉強の為の場所作ったり触ってみたりしただけ

---
+++ Assets/roro/test_macro.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_macro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	[macro name=FadeInBgm]
+
+	//フェードイン再生開始　3秒かけて徐々に再生されます[p]
+	[playbgm time="3" storage=test.mp3 loop=true]
+	[endmacro]
+
+
+
+}
