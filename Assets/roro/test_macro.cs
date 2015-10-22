d3a7aef82be54e345e0a47a4c6c1181b6712bc65
松山 和矢 <rorokage@gmail.com>
2015-10-22 11:34:40 +0900

    エラー消しました

--- Assets/roro/test_macro.cs
+++ Assets/roro/test_macro.cs
@@ -12,12 +12,12 @@ public class test_macro : MonoBehaviour {
 	void Update () {
 	
 	}
-	[macro name=FadeInBgm]
+/*	[macro name=FadeInBgm]
 
 	//フェードイン再生開始　3秒かけて徐々に再生されます[p]
 	[playbgm time="3" storage=test.mp3 loop=true]
 	[endmacro]
 
-
+*/
 
 }
